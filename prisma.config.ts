2abc03128797292fad9c6b7bf17499253415beed
Dede Iskandar <oborooodagiri@gmail.com>
2026-02-19 21:54:13 +0700

    Fix: Menambahkan semua folder dan file yang tertinggal

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,13 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+import { defineConfig } from "prisma/config";
+
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
-  },
+ schema: "prisma/schema.prisma",
+ datasource: {
+ db: {
+ url: process.env.DATABASE_URL!,
+ },
+ },
 });
