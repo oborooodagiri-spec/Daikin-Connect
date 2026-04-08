f9604130a334434a90a1dd3b8b8404b77e76188e
oborooodagiri-spec <oborooodagiri@gmail.com>
2026-04-08 08:09:51 +0700

    chore: version bump to 1.8.0 / V1.8.20260408

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "V1.8.20260407";
+export const APP_VERSION = "V1.8.20260408";
 export const VERSION = APP_VERSION; // Compatibility alias
 export const APP_EDITION = "Value Engineering Services";
 export const COPYRIGHT_YEAR = "2026";
