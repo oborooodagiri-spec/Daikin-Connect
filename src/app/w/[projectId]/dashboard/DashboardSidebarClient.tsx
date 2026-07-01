44966b487967f2c9c9a2136b1b44c7f4f822c974
oborooodagiri-spec <oborooodagiri@gmail.com>
2026-07-01 21:04:29 +0700

    style: increase sidebar logo size 3x

--- src/app/w/[projectId]/dashboard/DashboardSidebarClient.tsx
+++ src/app/w/[projectId]/dashboard/DashboardSidebarClient.tsx
@@ -110,7 +110,7 @@ export default function DashboardSidebarClient({
         ${isOpen ? "translate-x-0" : "-translate-x-full"} md:translate-x-0
       `}>
         <div className="p-8 border-b border-[#f7f8fa] flex flex-col items-center shrink-0">
-          <div className="relative h-14 lg:h-16 w-52 mb-6">
+          <div className="relative h-24 lg:h-28 w-full max-w-[240px] mb-6">
             <Image 
               src="/logo_epl_connect_1.png" 
               alt="EPL Link" 
